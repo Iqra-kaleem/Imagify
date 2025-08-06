3cfba5567fa09283567bff7f2aafd50ce29fea8a
Iqra Kaleem <iqrakaleem7054@gmail.com>
2025-08-06 13:54:12 +0530

    Animation in login, result, buy pages

--- client/src/pages/Result.jsx
+++ client/src/pages/Result.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import { motion } from 'motion/react';
 
 const Result = () => {
 
@@ -13,7 +14,12 @@ const Result = () => {
   }
 
   return (
-    <form onSubmit={onSubmithandler} className='flex flex-col min-h-[90vh] justify-center
+    <motion.form 
+    initial={{opacity:0.2, y:100}}
+    transition={{duration:1}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once: true}}
+    onSubmit={onSubmithandler} className='flex flex-col min-h-[90vh] justify-center
     items-center'>
     <div>
       <div className='relative'>
@@ -49,7 +55,7 @@ const Result = () => {
     </div>
       }
 
-    </form>
+    </motion.form>
   )
 }
 
